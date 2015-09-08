8b3f71120e73516c20201b77c1e53277b92f59ad
Boy Voesten <warrock.boy@hotmail.com>
2015-09-08 11:19:48 +0200

    NPC Sell list Prototype

---
+++ Assets/Scripts/Weapons/Stats.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+
+    [SerializeField] private string _name = "PlaceHolder";
+    [SerializeField] private float _price = 1f;
+    [SerializeField] private float _damage = 1f;
+
+    public string name
+    {
+        get
+        {
+            return _name;
+        }
+    }
+
+    public float price
+    {
+        get
+        {
+            return _price;
+        }
+    }
+
+
+    public float damage
+    {
+        get
+        {
+            return _damage;
+        }
+    }
+
+}
