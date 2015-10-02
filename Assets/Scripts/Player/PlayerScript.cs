9b26a29e98f5c74a767b0390889ed1b09e7f27e2
BeauArthurs <B.ArthursGames@gmail.com>
2015-10-02 09:23:34 +0100

    Beau animations update

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -9,27 +9,45 @@ public class PlayerScript : BaseCharacter {
 
 
 	private Quaternion targetRotation;
-	private int turningRate = 200;
-	private UIScript ui;
+	private int turningRate = 400;
 	void FixedUpdate ()
 	{
 		float h = Input.GetAxisRaw ("Horizontal");
 		float v = Input.GetAxisRaw ("Vertical");
 		if (h != 0 || v != 0) {
 			movement.Set (h, 0f, v);
-			Debug.Log(movement);
 			movement = transform.TransformDirection (movement);
 
 			body.MovePosition (transform.position + movement * Time.deltaTime * moveSpeed);
 			transform.rotation = Quaternion.RotateTowards (transform.rotation, targetRotation, turningRate * Time.deltaTime);
+            anime.SetBool("Moving", false);
+        }
+        else
+        {
+            anime.SetBool("Moving", true);
+        }
+        anime.SetFloat("DirX", h);
+        anime.SetFloat("DirY", v);
+        if (Input.GetAxis("Jump") > 0 && jumping == false)
+		{
+			Jump();
 		}
-
+        if(Input.GetMouseButtonDown(0))
+        {
+            anime.SetBool("Attack", true);
+        }
+		
 	}
+    public void EndAttack()
+    {
+        anime.SetBool("Attack", false);
+    }
 	public void SetRotation(Quaternion angles)
 	{
 		targetRotation = new Quaternion(0,angles.y,0,angles.w);
 	}
-	private void  OnCollisionEnter(Collision other)
+	public override void OnCollisionEnter(Collision other)
 	{
+		base.OnCollisionEnter (other);
 	}
 }
